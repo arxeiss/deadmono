afb1333f00a52af0d5d4759ab99267f5d967b37b
Pavel Kutáč <pavel.kutac@gmail.com>
2026-01-29 00:27:31 +0100

    Add JSON output support (#4)

--- analysis/run.go
+++ analysis/run.go
@@ -2,6 +2,7 @@ package analysis
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"io"
 	"os/exec"
@@ -17,12 +18,14 @@ type (
 		writer    io.Writer
 		errWriter io.Writer
 
-		lastModule string
-
 		// TagsFlag is a comma-separated list of extra build tags.
 		TagsFlag string
+		// FilterFlag is a regular expression to filter packages by.
+		FilterFlag string
 
-		paths []string
+		commonModule    string
+		paths           []string
+		hasCommonModule bool
 
 		// DebugFlag turns on more verbose output.
 		DebugFlag bool
@@ -36,7 +39,13 @@ type (
 
 	entrypointInfo struct {
 		deps     map[string]struct{}
-		deadCode map[string]map[string]struct{}
+		deadCode map[string]deadPackageFuncs
+		absPath  string
+	}
+
+	deadPackageFuncs struct {
+		pkg   *Package
+		funcs map[string]*Function
 	}
 )
 
@@ -65,35 +74,36 @@ func (r *Runner) Run(ctx context.Context) error {
 	if len(r.paths) == 0 {
 		return fmt.Errorf("no paths provided")
 	}
-	if err := r.verifyBinaries(ctx); err != nil {
+	err := r.verifyBinaries(ctx)
+	if err != nil {
 		return err
 	}
 
+	// Collect as much information as possible about entrypoints before we start scanning for deadcode.
 	eps := make([]*entrypointInfo, 0, len(r.paths))
 	for _, path := range r.paths {
-		ep, err := r.scanEntrypoint(ctx, path)
+		var ep *entrypointInfo
+		ep, err = r.scanEntrypoint(ctx, path)
 		if err != nil {
 			return err
 		}
 		eps = append(eps, ep)
 	}
 
-	deadCode := r.intersectDeadcode(eps)
-	if r.JSONFlag {
-		return r.printJSON(ctx, deadCode)
-	}
-
-	allPaths := make([]string, 0)
-	for _, lines := range deadCode {
-		for line := range lines {
-			allPaths = append(allPaths, line)
+	// Scan for deadcode.
+	for _, ep := range eps {
+		ep.deadCode, err = r.listEntrypointDeadCode(ctx, ep.absPath)
+		if err != nil {
+			return err
 		}
 	}
 
-	slices.Sort(allPaths)
-	for _, path := range allPaths {
-		fmt.Fprintln(r.writer, path)
+	deadCode := r.intersectDeadCode(eps)
+
+	if r.JSONFlag {
+		return r.printJSON(ctx, deadCode)
 	}
+	r.printText(ctx, deadCode)
 
 	return nil
 }
@@ -111,11 +121,6 @@ func (r *Runner) scanEntrypoint(ctx context.Context, path string) (*entrypointIn
 	}
 
 	ep, err := r.listDependencies(ctx, absPath)
-	if err != nil {
-		return nil, err
-	}
-
-	ep.deadCode, err = r.listEntrypointDeadcode(ctx, absPath)
 	if err != nil {
 		return nil, err
 	}
@@ -144,12 +149,21 @@ func (r *Runner) verifyModule(ctx context.Context, absPath string) error {
 		return fmt.Errorf("failed to list module name: %w", err)
 	}
 
-	moduleName := strings.TrimSuffix(strings.TrimSpace(string(out)), "/") + "/"
-	if r.lastModule != "" && r.lastModule != moduleName {
-		return fmt.Errorf("different modules in one run are not supported: %s != %s", r.lastModule, moduleName)
+	m := strings.TrimSuffix(strings.TrimSpace(string(out)), "/") + "/"
+	switch {
+	case r.commonModule == "":
+		r.commonModule = m
+		r.hasCommonModule = true
+	case r.commonModule == m:
+		// Do nothing, as everything was set before
+	case r.FilterFlag == "<module>" || r.FilterFlag == "":
+		// If we have custom filter, we don't need to have same module, as we will filter by regexp anyway.
+		// If flag is <module>, we need all scans to be within same module, otherwise intersection will be empty.
+		return fmt.Errorf("different modules are not supported without filter flag: %s != %s", r.commonModule, m)
+	default:
+		r.hasCommonModule = false
 	}
-	r.lastModule = moduleName
-	r.writeDebug("Detected module name: %s", r.lastModule)
+	r.writeDebug("Detected module name: %s", r.commonModule)
 	return nil
 }
 
@@ -160,18 +174,37 @@ func (r *Runner) listDependencies(ctx context.Context, absPath string) (*entrypo
 	}
 
 	ep := &entrypointInfo{
-		deps: make(map[string]struct{}),
+		absPath: absPath,
+		deps:    make(map[string]struct{}),
 	}
 	for _, line := range strings.Split(string(out), "\n") {
-		if m, found := strings.CutPrefix(line, r.lastModule); found {
-			ep.deps[m] = struct{}{}
+		if line == "" {
+			continue
 		}
+		ep.deps[line] = struct{}{}
 	}
 	r.writeDebug("Detected %d dependencies", len(ep.deps))
 	return ep, nil
 }
 
-func (r *Runner) listEntrypointDeadcode(ctx context.Context, absPath string) (map[string]map[string]struct{}, error) {
+func (r *Runner) getDeadCodeArgs() []string {
+	args := []string{"-json"}
+	if r.GeneratedFlag {
+		args = append(args, "-generated")
+	}
+	if r.TestFlag {
+		args = append(args, "-test")
+	}
+	if r.TagsFlag != "" {
+		args = append(args, "-tags", r.TagsFlag)
+	}
+	if r.FilterFlag != "" {
+		args = append(args, "-filter", r.FilterFlag)
+	}
+	return append(args, "./...")
+}
+
+func (r *Runner) listEntrypointDeadCode(ctx context.Context, absPath string) (map[string]deadPackageFuncs, error) {
 	absDirPath := filepath.Dir(absPath)
 	out, err := getCommandOutput(ctx, absDirPath, "go", "list", "-f", `{{.Root}}`)
 	if err != nil {
@@ -182,75 +215,80 @@ func (r *Runner) listEntrypointDeadcode(ctx context.Context, absPath string) (ma
 
 	r.writeDebug("Starting to scan %s for deadcode, might take a while", absDirPath)
 	timeStart := time.Now()
-	args := []string{"-f", `{{range .Funcs}}{{printf "%s: unreachable func: %s\n" .Position .Name}}{{end}}`}
-	if r.GeneratedFlag {
-		args = append(args, "-generated")
-	}
-	if r.TestFlag {
-		args = append(args, "-test")
-	}
-	if r.TagsFlag != "" {
-		args = append(args, "-tags", r.TagsFlag)
-	}
-	out, err = getCommandOutput(ctx, absDirPath, "deadcode", append(args, "./...")...)
+
+	out, err = getCommandOutput(ctx, absDirPath, "deadcode", r.getDeadCodeArgs()...)
 	if err != nil {
 		return nil, fmt.Errorf("failed to list deadcode: %w", err)
 	}
 	r.writeDebug("Scanning %s for deadcode finished in %s", absDirPath, time.Since(timeStart))
 
-	deadCode := map[string]map[string]struct{}{}
-	for _, line := range strings.Split(string(out), "\n") {
-		var found bool
-		line, found = strings.CutPrefix(line, rootPath)
-		if !found {
-			line, _ = strings.CutPrefix(filepath.Join(absDirPath, line), rootPath)
-		}
-		filePath, _, found := strings.Cut(line, ":")
-		if !found {
-			continue
-		}
-
-		pkg := filepath.Dir(filePath)
-		if deadCode[pkg] == nil {
-			deadCode[pkg] = make(map[string]struct{})
-		}
-		deadCode[pkg][line] = struct{}{}
+	deadCode := map[string]deadPackageFuncs{}
+	pkgs := make([]*Package, 0)
+	if err := json.Unmarshal(out, &pkgs); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal deadcode output: %w", err)
 	}
+	for _, pkg := range pkgs {
+		dpf := deadPackageFuncs{
+			pkg:   pkg,
+			funcs: make(map[string]*Function),
+		}
+		for _, fun := range pkg.Funcs {
+			f := fun.Position.File
+			// If path is not absolute, it means it is relative to the module root.
+			// Because we have many entrypoints, we need to make sure we have absolute paths.
+			if !filepath.IsAbs(f) {
+				f = filepath.Join(absDirPath, f)
+			}
+			// If all entrypoints are within same module, we can remove the path to module root from the file path.
+			// Then it will be relative to go.mod file.
+			if r.hasCommonModule {
+				f, _ = strings.CutPrefix(f, rootPath)
+			}
+			fun.Position.File = f // Override back, so we have consistent output.
+			dpf.funcs[fun.Name] = fun
+		}
+		pkg.Funcs = nil // Clear just not to use it accidentally.
+		deadCode[pkg.Path] = dpf
+	}
+
 	return deadCode, nil
 }
 
-func (*Runner) intersectDeadcode(eps []*entrypointInfo) map[string]map[string]struct{} {
+func (*Runner) intersectDeadCode(eps []*entrypointInfo) map[string]deadPackageFuncs {
 	result := eps[0]
 	for pkg := range result.deps {
 		if _, found := result.deadCode[pkg]; !found {
 			// If pkg is using all functions inside (has no records in deadcode), we should mark that to result.
 			// This is special case, but we need to keep track of it.
 			result.deps[pkg] = struct{}{}
-			result.deadCode[pkg] = make(map[string]struct{})
+			result.deadCode[pkg] = deadPackageFuncs{}
 		}
 	}
 
 	for _, ep := range eps[1:] {
-		for pkg, lines := range ep.deadCode {
+		for pkg, dpf := range ep.deadCode {
 			// Iterating entrypoint uses package, that wasn't scanned yet,
 			// just add all what is unused in iterating entrypoint.
 			_, visitedDep := result.deps[pkg]
 			// Check deadcode in result already, if not exists too, same situation as above
-			resultDeadLines, exists := result.deadCode[pkg]
+			resultDpf, exists := result.deadCode[pkg]
 
 			if !exists || !visitedDep {
 				result.deps[pkg] = struct{}{}
-				result.deadCode[pkg] = lines
+				result.deadCode[pkg] = dpf
 				continue
 			}
 
 			// Package was already scanned, so we need to intersect the deadcode.
-			// Only keep lines that appear in both maps.
-			intersection := make(map[string]struct{})
-			for line := range resultDeadLines {
+			// Only keep functions that appear in both maps.
+			intersection := deadPackageFuncs{
+				pkg:   resultDpf.pkg,
+				funcs: make(map[string]*Function),
+			}
+			for fnName := range resultDpf.funcs {
 				// If what we consider unused so far exists in current entrypoint too, keep it.
-				if _, found := lines[line]; found {
-					intersection[line] = struct{}{}
+				if fun, found := dpf.funcs[fnName]; found {
+					intersection.funcs[fnName] = fun
 				}
 			}
 			result.deadCode[pkg] = intersection
@@ -259,15 +297,58 @@ func (*Runner) intersectDeadcode(eps []*entrypointInfo) map[string]map[string]st
 		for pkg := range ep.deps {
 			// If our pkg is using all functions inside (has no records in deadcode), we should mark that to result.
 			// This is special case, but we need to keep track of it.
-			if len(ep.deadCode[pkg]) == 0 {
+			if len(ep.deadCode[pkg].funcs) == 0 {
 				result.deps[pkg] = struct{}{}
-				result.deadCode[pkg] = make(map[string]struct{})
+				result.deadCode[pkg] = deadPackageFuncs{}
 			}
 		}
 	}
 	return result.deadCode
 }
 
-func (*Runner) printJSON(_ context.Context, _ map[string]map[string]struct{}) error {
-	return fmt.Errorf("JSON output is not implemented yet")
+func (r *Runner) printJSON(_ context.Context, deadCode map[string]deadPackageFuncs) error {
+	out := make([]*Package, 0, len(deadCode))
+
+	for _, dpf := range deadCode {
+		if len(dpf.funcs) == 0 {
+			continue
+		}
+		dpf.pkg.Funcs = make([]*Function, 0, len(dpf.funcs))
+		for _, fun := range dpf.funcs {
+			dpf.pkg.Funcs = append(dpf.pkg.Funcs, fun)
+		}
+		slices.SortFunc(dpf.pkg.Funcs, func(a, b *Function) int {
+			s := strings.Compare(a.Position.File, b.Position.File)
+			if s != 0 {
+				return s
+			}
+			return a.Position.Line - b.Position.Line
+		})
+
+		out = append(out, dpf.pkg)
+	}
+	slices.SortFunc(out, func(a, b *Package) int {
+		return strings.Compare(a.Path, b.Path)
+	})
+
+	enc := json.NewEncoder(r.writer)
+	enc.SetIndent("", "\t")
+	return enc.Encode(out)
+}
+
+func (r *Runner) printText(_ context.Context, deadCode map[string]deadPackageFuncs) {
+	allPaths := make([]string, 0)
+	for _, dpf := range deadCode {
+		for _, fun := range dpf.funcs {
+			allPaths = append(allPaths, fmt.Sprintf(
+				"%s:%d:%d: unreachable func: %s",
+				fun.Position.File, fun.Position.Line, fun.Position.Col, fun.Name,
+			))
+		}
+	}
+
+	slices.Sort(allPaths)
+	for _, path := range allPaths {
+		fmt.Fprintln(r.writer, path)
+	}
 }
--- analysis/run_test.go
+++ analysis/run_test.go
@@ -3,13 +3,16 @@ package analysis_test
 import (
 	"bytes"
 	"context"
+	"encoding/json"
 	"path/filepath"
 	"slices"
+	"strings"
 
 	"github.com/arxeiss/deadmono/analysis"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gstruct"
 )
 
 var _ = Describe("Runner", func() {
@@ -41,10 +44,24 @@ var _ = Describe("Runner", func() {
 
 	It("fails on different go modules", func() {
 		ctx := context.Background()
-		r := analysis.New(stdOut, stdErr, []string{"testdata/allinone/services/authn/main.go", "/home"})
+		r := analysis.New(stdOut, stdErr, []string{
+			"testdata/allinone/services/authn/main.go",
+			"testdata/cli/main.go",
+		})
 		err := r.Run(ctx)
-		Expect(err).To(MatchError(
-			"different modules in one run are not supported: github.com/arxeiss/deadmono/ != command-line-arguments/"))
+		Expect(err).To(MatchError("different modules are not supported without filter flag: " +
+			"github.com/arxeiss/deadmono/ != github.com/arxeiss/deadmono/testdata/"))
+	})
+
+	It("fails on invalid filter flag", func() {
+		ctx := context.Background()
+		r := analysis.New(stdOut, stdErr, []string{"testdata/allinone/services/authn/main.go"})
+		r.FilterFlag = "*"
+		err := r.Run(ctx)
+		Expect(err).To(MatchError(HavePrefix(
+			//nolint:dupword // no duplicate word, but real error
+			"failed to list deadcode: deadcode: -filter: error parsing regexp: missing argument to repetition operator",
+		)))
 	})
 
 	DescribeTable("Verify all in one example",
@@ -98,7 +115,7 @@ var _ = Describe("Runner", func() {
 		Expect(stdErr.String()).To(HavePrefix(
 			"Start scanning entrypoint: " + dir + "analysis/testdata/allinone/services/authn/main.go\n" +
 				"Detected module name: github.com/arxeiss/deadmono/\n" +
-				"Detected 3 dependencies\n" +
+				"Detected 30 dependencies\n" +
 				"Detected root path: " + dir + "\n" +
 				"Starting to scan " + dir + "analysis/testdata/allinone/services/authn for deadcode, might take a while\n" +
 				"Scanning " + dir + "analysis/testdata/allinone/services/authn for deadcode finished in ",
@@ -137,4 +154,142 @@ var _ = Describe("Runner", func() {
 			"analysis/testdata/allinone/pkg/logging/logging.go:12:6: unreachable func: Warn\n"+
 			"analysis/testdata/allinone/pkg/logging/logging.go:6:6: unreachable func: Debug\n"),
 	)
+
+	It("Handles properly multiple modules with filter", func() {
+		ctx := context.Background()
+		r := analysis.New(stdOut, stdErr, []string{
+			"testdata/allinone/services/config/main.go",
+			"testdata/cli/main.go",
+		})
+		r.FilterFlag = "Masterminds/semver"
+		Expect(r.Run(ctx)).To(Succeed())
+
+		lines := strings.Split(strings.TrimSpace(stdOut.String()), "\n")
+		foundStrictNewVersion, foundMustParse := "", ""
+		for _, line := range lines {
+			if strings.Contains(line, "StrictNewVersion") {
+				foundStrictNewVersion = line
+			} else if strings.Contains(line, "MustParse") {
+				foundMustParse = line
+			}
+			Expect(line).To(ContainSubstring("github.com/!masterminds/semver/v3@"))
+		}
+		Expect(foundStrictNewVersion).To(BeEmpty(), "StrictNewVersion should not be found")
+		Expect(foundMustParse).To(BeEmpty(), "MustParse should not be found")
+	})
+
+	It("Handles JSON output", func() {
+		ctx := context.Background()
+		r := analysis.New(stdOut, stdErr, []string{
+			"testdata/allinone/services/authn/main.go",
+			"testdata/allinone/services/config/main.go",
+			"testdata/allinone/services/healthcheck/main.go",
+		})
+		r.JSONFlag = true
+		r.GeneratedFlag = true
+		Expect(r.Run(ctx)).To(Succeed())
+
+		expected := []*analysis.Package{
+			{
+				Name: "cache",
+				Path: "github.com/arxeiss/deadmono/analysis/testdata/allinone/pkg/cache",
+				Funcs: []*analysis.Function{
+					{
+						Name: "Delete",
+						Position: analysis.Position{
+							File: "analysis/testdata/allinone/pkg/cache/cache.go",
+							Line: 12,
+							Col:  6,
+						},
+					},
+				},
+			},
+			{
+				Name: "logging",
+				Path: "github.com/arxeiss/deadmono/analysis/testdata/allinone/pkg/logging",
+				Funcs: []*analysis.Function{
+					{
+						Name: "Debug",
+						Position: analysis.Position{
+							File: "analysis/testdata/allinone/pkg/logging/logging.go",
+							Line: 6,
+							Col:  6,
+						},
+					},
+					{
+						Name: "Warn",
+						Position: analysis.Position{
+							File: "analysis/testdata/allinone/pkg/logging/logging.go",
+							Line: 12,
+							Col:  6,
+						},
+					},
+				},
+			},
+			{
+				Name: "internal",
+				Path: "github.com/arxeiss/deadmono/analysis/testdata/allinone/services/authn/internal",
+				Funcs: []*analysis.Function{
+					{
+						Name: "RunFromTest",
+						Position: analysis.Position{
+							File: "analysis/testdata/allinone/services/authn/internal/auth.go",
+							Line: 18,
+							Col:  6,
+						},
+					},
+					{
+						Name: "Generated",
+						Position: analysis.Position{
+							File: "analysis/testdata/allinone/services/authn/internal/generated.go",
+							Line: 5,
+							Col:  6,
+						},
+						Generated: true,
+					},
+				},
+			},
+		}
+
+		expectedOut, err := json.MarshalIndent(expected, "", "\t")
+		Expect(err).To(Succeed())
+		Expect(stdOut.Bytes()).To(MatchJSON(expectedOut))
+	})
+
+	It("Handles JSON output with different modules", func() {
+		ctx := context.Background()
+		r := analysis.New(stdOut, stdErr, []string{
+			"testdata/allinone/services/config/main.go",
+			"testdata/cli/main.go",
+		})
+		r.JSONFlag = true
+		r.FilterFlag = "Masterminds/semver"
+		Expect(r.Run(ctx)).To(Succeed())
+
+		var out []*analysis.Package
+		err := json.Unmarshal(stdOut.Bytes(), &out)
+		Expect(err).To(Succeed())
+
+		Expect(out).To(HaveLen(1))
+		Expect(out[0].Path).To(Equal("github.com/Masterminds/semver/v3"))
+		Expect(out[0].Name).To(Equal("semver"))
+
+		Expect(out[0].Funcs).To(ContainElement(PointTo(MatchAllFields(Fields{
+			"Name": Equal("NewConstraint"),
+			"Position": MatchAllFields(Fields{
+				"File": ContainSubstring("github.com/!masterminds/semver/v3@"),
+				"Line": BeNumerically(">", 0),
+				"Col":  BeNumerically(">", 0),
+			}),
+			"Generated": BeFalse(),
+			"Marker":    BeFalse(),
+		}))))
+
+		Expect(out[0].Funcs).NotTo(ContainElement(PointTo(MatchAllFields(Fields{
+			"Name": Equal("StrictNewVersion"),
+		}))))
+		Expect(out[0].Funcs).NotTo(ContainElement(PointTo(MatchAllFields(Fields{
+			"Name": Equal("MustParse"),
+		}))))
+	})
 })
